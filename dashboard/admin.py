f3066d14098832f9422ebef1c3568ec928f63ed8
prosper-20 <edwardprosper001@gmail.com>
2022-01-15 18:59:07 +0100

    updates

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quantity')
@@ -9,6 +9,6 @@ admin.site.site_header = "Prosper's Inventory"
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
-# admin.site.register(Order)
+admin.site.register(Order)
 
 
--- dashboard/models.py
+++ dashboard/models.py
@@ -23,3 +23,11 @@ class Product(models.Model):
 
 
 
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
