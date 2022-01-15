74ebb1ab2e36a6c0574cc51650c0b22b3535f58a
prosper-20 <edwardprosper001@gmail.com>
2022-01-15 18:36:36 +0100

    updates

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Product, Order
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity')
+
+admin.site.site_header = "Prosper's Inventory"
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
+
+
