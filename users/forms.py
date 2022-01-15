9bd1b39c63d2ad8b475047343fc6b6850d706289
prosper-20 <edwardprosper001@gmail.com>
2022-01-15 20:28:44 +0100

    updates

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import fields
+import imp
+from socket import fromshare
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(help_text="Enter a valid email address")
+
+    class Meta:
+        model = User
+
+    
+        fields = ["username", "email", "password1", "password2"]
--- users/views.py
+++ users/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Hi {username}, your account has been created successfully!")
             return redirect("home")
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     context = {
         "form": form
