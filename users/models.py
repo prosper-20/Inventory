20a72a95abfb677b1f65e498b3e03ae2ac85f1f2
prosper-20 <edwardprosper001@gmail.com>
2022-01-16 21:14:00 +0100

    updates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
+from re import U
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="user.jpg", upload_to="PROFILE_PICS")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
